29aaf07da04a741c8cf7de3f39db49f5b6d75b3a
Apostolos Bessas <mpessas@transifex.com>
2012-12-11 17:41:57 +0200

    Bump to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='django-bulk',
-    version='0.1devel',
+    version='0.1.1',
     author='Kevin Mahoney',
     author_email='kevin.mahoney@maplecroft.com',
     packages=['djangobulk'],
