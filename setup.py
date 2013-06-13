4c944d73611fbf5d209ca6b8be725c7dd93807f7
Diego Búrigo Zacarão <diegobz@transifex.com>
2013-06-13 12:23:04 -0300

    Bump version to 0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='django-bulk',
-    version='0.1.1',
+    version='0.1.2',
     author='Kevin Mahoney',
     author_email='kevin.mahoney@maplecroft.com',
     packages=['djangobulk'],
